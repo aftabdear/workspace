17ee212debb3ef948f9151bd15820017fddc6014
AftabComputer <aftabdear@googlemail.com>
2017-11-13 17:44:37 +0000

    v1.0

---
+++ 7QPMule/src/FailSafe1.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+import org.osbot.rs07.api.map.constants.Banks;
+import org.osbot.rs07.script.Script;
+
+public class FailSafe1 extends Task{
+
+	public FailSafe1(Script script) {
+		super(script);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean verify() {
+		// TODO Auto-generated method stub
+		return Areas.failSafe1.contains(script.myPlayer().getPosition());
+	}
+
+	@Override
+	public int execute() throws Exception {
+		script.log("in fail safe 1");
+		if (script.getWalking().walkPath(Arrays.asList(Areas.pathMineToBank1))){
+			Sleep.sleepUntil(()-> Banks.FALADOR_EAST.contains(script.myPlayer().getPosition()), 10000);
+		}
+		
+		
+		return 100;
+	}
+
+	@Override
+	public String describe() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ 7QPMule/src/Task.java
@@ -0,0 +1,42 @@
+import org.osbot.rs07.api.ui.Message;
+import org.osbot.rs07.script.Script;
+
+public abstract class Task {
+  // The script instance
+  protected Script script;
+
+  public Task(Script script) {
+    this.script = script;
+  }
+
+  /**
+   * @return if this Task should execute.
+   */
+  public abstract boolean verify();
+
+  /**
+   * Executes this Task.
+   *
+   * @return sleep time after this task ends.
+ * @throws Exception 
+   */
+  
+
+  
+  
+  
+  
+  
+  
+  public abstract int execute() throws Exception;
+
+  /**
+   * @return a description of the current Task.
+   */
+  public abstract String describe();
+
+
+
+
+
+}
