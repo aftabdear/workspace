db4517d5eed2a222a64b037fb3580deec26fb268
AftabComputer <aftabdear@googlemail.com>
2017-11-22 13:52:42 +0000

    1.1

--- EmblemFarmer/src/AssignedATarget.java
+++ EmblemFarmer/src/AssignedATarget.java
@@ -1,7 +1,9 @@
+import org.osbot.rs07.api.model.Item;
 import org.osbot.rs07.api.ui.RS2Widget;
 import org.osbot.rs07.script.Script;
 
 public class AssignedATarget extends Task{
+	public static String TargetName;
 
 	public AssignedATarget(Script script) {
 		super(script);
@@ -22,15 +24,59 @@ public class AssignedATarget extends Task{
 		script.log("In the AssignedATarget class");
 		script.log("We have been assigned a Target");
 		
+		Item tier1 = script.getInventory().getItem("Mysterious emblem");
+		Item tier2 = script.getInventory().getItem("Mysterious emblem (tier 2)");
+		Item tier3 = script.getInventory().getItem("Mysterious emblem (tier 3)");
+		Item tier4 = script.getInventory().getItem("Mysterious emblem (tier 4)");
+		Item tier5 = script.getInventory().getItem("Mysterious emblem (tier 5)");
+		Item tier6 = script.getInventory().getItem("Mysterious emblem (tier 6)");
+		Item tier7 = script.getInventory().getItem("Mysterious emblem (tier 7)");
+		Item tier8 = script.getInventory().getItem("Mysterious emblem (tier 8)");
+		Item tier9 = script.getInventory().getItem("Mysterious emblem (tier 8)");
+		Item tier10 = script.getInventory().getItem("Mysterious emblem (tier 10)");
+
+		
 		RS2Widget Target = script.getWidgets().get(90, 37);
-		String TargetName = Target.getMessage();
+		TargetName = Target.getMessage();
 		
 		script.log("Currently assigned to " + TargetName);
 		
 		Main.getResponseForString("$"+TargetName);
 		
+		if (tier1 != null){
+			Main.getResponseForString("£-1-"+TargetName);
+		}
+		if (tier2 != null){
+			Main.getResponseForString("£-2-"+TargetName);
+		}
+		if (tier3 != null){
+			Main.getResponseForString("£-3-"+TargetName);
+		}
+		if (tier4 != null){
+			Main.getResponseForString("£-4-"+TargetName);
+		}
+		if (tier5 != null){
+			Main.getResponseForString("£-5-"+TargetName);
+		}
+		if (tier6 != null){
+			Main.getResponseForString("£-6-"+TargetName);
+		}
+		if (tier7 != null){
+			Main.getResponseForString("£-7-"+TargetName);
+		}
+		if (tier8 != null){
+			Main.getResponseForString("£-8-"+TargetName);
+		}
+		if (tier9 != null){
+			Main.getResponseForString("£-9-"+TargetName);
+		}
 		
-		return 1000;
+		if (tier10 != null){
+			Main.getResponseForString("£-10-"+TargetName);
+		}
+		
+		
+		return 10000;
 	}
 
 	@Override
--- EmblemFarmer/src/Main.java
+++ EmblemFarmer/src/Main.java
@@ -42,17 +42,22 @@ public class Main extends Script implements MessageListener {
 	public static LinkedList<String> accounts = new LinkedList<String>();
 	public static Iterator<String> it;
 
-	//tcp socket stuff
+	// tcp socket stuff
 	static Socket s1 = null;
-    static BufferedReader is = null;
-    static PrintWriter os = null;
-    BotClientListener clientListener = null;
-    
-    Item tier1 = getInventory().getItem("Mysterious emblem");
-    Item tier2 = getInventory().getItem("Mysterious emblem (tier 2)");
+	static BufferedReader is = null;
+	static PrintWriter os = null;
+	BotClientListener clientListener = null;
 
-
-
+	Item tier1 = getInventory().getItem("Mysterious emblem");
+	Item tier2 = getInventory().getItem("Mysterious emblem (tier 2)");
+	Item tier3 = getInventory().getItem("Mysterious emblem (tier 3)");
+	Item tier4 = getInventory().getItem("Mysterious emblem (tier 4)");
+	Item tier5 = getInventory().getItem("Mysterious emblem (tier 5)");
+	Item tier6 = getInventory().getItem("Mysterious emblem (tier 6)");
+	Item tier7 = getInventory().getItem("Mysterious emblem (tier 7)");
+	Item tier8 = getInventory().getItem("Mysterious emblem (tier 8)");
+	Item tier9 = getInventory().getItem("Mysterious emblem (tier 8)");
+	Item tier10 = getInventory().getItem("Mysterious emblem (tier 10)");
 
 	@Override
 	public void onStart() throws InterruptedException {
@@ -60,131 +65,165 @@ public class Main extends Script implements MessageListener {
 		tasks.add(new WaitingForTarget(this));
 		tasks.add(new AssignedATarget(this));
 		tasks.add(new TargetWidgetIsNotVisible(this));
-		
-		
+
 		startTime = System.currentTimeMillis();
-		
-		
+
 		try {
 			sleep(5000);
-            s1 = new Socket("localhost", 1337); 
-            is = new BufferedReader(new InputStreamReader(s1.getInputStream()));
-            os = new PrintWriter(s1.getOutputStream());
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (s1.isConnected()) { 
-           // getResponseForString("USERNAME:" + myPlayer().getName().toString()); //think it'll connect to server now aight sec
-            clientListener = new BotClientListener();
-            clientListener.start();
-        } else {
-            stop();
-        }
+			s1 = new Socket("localhost", 1337);
+			is = new BufferedReader(new InputStreamReader(s1.getInputStream()));
+			os = new PrintWriter(s1.getOutputStream());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (s1.isConnected()) {
+			// getResponseForString("USERNAME:" +
+			// myPlayer().getName().toString()); //think it'll connect to server
+			// now aight sec
+			clientListener = new BotClientListener();
+			clientListener.start();
+		} else {
+			stop();
+		}
 	}
 
 	@Override
-	public int onLoop() throws InterruptedException { //I think that's it
-		
-				for (Task task : tasks) {
-					if (task.verify())
-						try {
-							return task.execute();
-						} catch (Exception e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
+	public int onLoop() throws InterruptedException { // I think that's it
+
+		for (Task task : tasks) {
+			if (task.verify())
+				try {
+					return task.execute();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-			
-		
+		}
+
 		return 150;
 	}
 
-	public static Integer arrayTiers[] = {
-		1,2,3,4,5,6,7,8 	
-	};
-	
-	
+	public static Integer arrayTiers[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
 	private void parseServerMessage(String message) {
-        log(message);
-        if (message.contains("$") && message.contains(myPlayer().getName().toString())) {
-        	
-        	//send the tier level you have
-        	getResponseForString("£"+""+ message.contains(myPlayer().getName().toString()));
-        	
-        }else if (message.contains("£-1")){
-         	String msg[] = message.split("-");
-         	String NA = msg[0];
-         	String Tier = msg[1];
-         	
-         	
-         	
-         }else if (message.contains("asdasd")) {
-          	
-          	
-          }else {
-            switch (message) {
-                case "LEVEL":
-                    break;
-            }
-        }
-    }
-	
-	//more like send message but named stupidly
-    public static void getResponseForString(String message) {
-        if (os == null) {
-            try {
-                s1 = new Socket("localhost", 1337);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                is = new BufferedReader(new InputStreamReader(s1.getInputStream()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                os = new PrintWriter(s1.getOutputStream());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        os.println(message);
-        os.flush();
+		log(message);
+		if (message.contains("\\$") && message.contains(myPlayer().getName().toString())) {
 
-    }
-    
-  //monitors messages from server and parses them
-    private class BotClientListener extends Thread {
-          
-  	  
-  	  @Override
-          public void run() {
-              InputStream is = null;
-              try {
-                  is = s1.getInputStream();
-              } catch (IOException e) {
-                  e.printStackTrace();
-                  return;
-              }
-              InputStreamReader isr = new InputStreamReader(is);
-              BufferedReader br = new BufferedReader(isr);
-              while (s1.isConnected() || s1.isBound()) {
-                  String message = null;
-                  try {
-                      message = br.readLine();
-                      parseServerMessage(message);
-                  } catch (IOException e) {
-                      e.printStackTrace();
-                  }
-              }
-          }
-      }
-	
+			if (tier1 != null) {
+				getResponseForString("£-1-" + message.contains(myPlayer().getName().toString()));
+			}
+			if (tier2 != null) {
+				getResponseForString("£-2" + "" + message.contains(myPlayer().getName().toString()));
+			}
+			if (tier3 != null) {
+				getResponseForString("£-3" + "-" + message.contains(myPlayer().getName().toString()));
+			}
+			if (tier4 != null) {
+				getResponseForString("£-4" + "-" + message.contains(myPlayer().getName().toString()));
+			}
+			if (tier5 != null) {
+				getResponseForString("£-5" + "-" + message.contains(myPlayer().getName().toString()));
+			}
+			if (tier6 != null) {
+				getResponseForString("£-6" + "-" + message.contains(myPlayer().getName().toString()));
+			}
+			if (tier7 != null) {
+				getResponseForString("£-7" + "-" + message.contains(myPlayer().getName().toString()));
+			}
+			if (tier8 != null) {
+				getResponseForString("£-8" + "-" + message.contains(myPlayer().getName().toString()));
+			}
+			if (tier9 != null) {
+				getResponseForString("£-9" + "-" + message.contains(myPlayer().getName().toString()));
+			}
+			if (tier10 != null) {
+				getResponseForString("£-10" + "-" + message.contains(myPlayer().getName().toString()));
+			}
 
-	
+		} else if (message.contains("£-1") && message.contains(myPlayer().getName().toString())) {
+			String msg[] = message.split("-");
+			String NA = msg[0];
+			String StringTier = msg[1];
+			String Username = msg[2];
+			
+			int Tier = Integer.parseInt(StringTier);
 
+			if (tier1 != null) {
+				log("we have a tier 1");
+				int oTier1 = 1;
+
+				if (Tier >= oTier1) {
+					log("They have a higher tier then us");
+				}
+				if (Tier == oTier1) {
+					log("They have the same tier as us");
+				}
+				if (Tier <= oTier1) {
+					log("They have a lower tier then us");
+				}
+			}
+
+		} else if (message.contains("asdasd")) {
+
+		} else {
+			switch (message) {
+			case "LEVEL":
+				break;
+			}
+		}
+	}
+
+	// more like send message but named stupidly
+	public static void getResponseForString(String message) {
+		if (os == null) {
+			try {
+				s1 = new Socket("localhost", 1337);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				is = new BufferedReader(new InputStreamReader(s1.getInputStream()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				os = new PrintWriter(s1.getOutputStream());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		os.println(message);
+		os.flush();
+
+	}
+
+	// monitors messages from server and parses them
+	private class BotClientListener extends Thread {
+
+		@Override
+		public void run() {
+			InputStream is = null;
+			try {
+				is = s1.getInputStream();
+			} catch (IOException e) {
+				e.printStackTrace();
+				return;
+			}
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			while (s1.isConnected() || s1.isBound()) {
+				String message = null;
+				try {
+					message = br.readLine();
+					parseServerMessage(message);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
 	@Override
 	public void onPaint(Graphics2D g) {
